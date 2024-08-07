848cef615e6ed87c4bcb756d3c0838a6ccf84e7c
ShubhamYv <shubhamyadav32100@gmail.com>
2024-08-07 18:11:11 +0530

    Add VideoUpload component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
+import VideoUpload from './components/VideoUpload'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold text-white">
-        Video Streaming App
-      </h1>
+      <div className='flex flex-col items-center space-y-9 justify-center py-10'>
+        <h1 className="text-5xl font-extrabold text-gray-700 dark:text-gray-100">
+          Welcome to video Streaming Application
+        </h1>
+
+        <VideoUpload />
+      </div>
     </>
   )
 }
